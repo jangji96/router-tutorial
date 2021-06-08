b50872689e17bca093fa236c4a35345a086d9b30
jangji96 <xxwlgns@naver.com>
2021-06-08 09:48:55 +0900

    파라미터와 쿼리

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Link, Route } from 'react-router-dom';
 import About from './About';
 import Home from './Home';
+import Profile from './Profile';
 
 function App() {
   return (
@@ -17,6 +18,7 @@ function App() {
       <hr />
       <Route path="/" exact={true} component={Home} />
       <Route path="/about" component={About} />
+      <Route path="/profiles/:username" component={Profile} />
     </div>
   );
 }
